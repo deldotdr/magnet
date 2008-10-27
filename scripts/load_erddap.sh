3d7af8fa3b53fa3aeeee2b6b69f3cddf2f272ac7
dorian <dorian@mcworld.(none)>
2008-10-27 16:30:25 -0700

    fixed load erddap script

--- scripts/load_erddap.sh
+++ scripts/load_erddap.sh
@@ -1,5 +1,5 @@
 wget -q http://amoeba.ucsd.edu/fileshare/apps/erddap-ooi-latest.tar.gz 
-tar zxf erddap-ooi-0.1.tar.gz
+tar zxf erddap-ooi-latest.tar.gz
 mv erddap-ooi /opt/apache-tomcat-6.0.18/webapps/
 mkdir /opt/apache-tomcat-6.0.18/content
 cp -r /opt/apache-tomcat-6.0.18/webapps/erddap-ooi/content.def/erddap /opt/apache-tomcat-6.0.18/content
