4868f266260bd2e2955aa82b27b8274ae4ee88bd
Paul Hubbard <pfhubbar@ucsd.edu>
2009-07-09 14:59:34 -0700

    fiddling with doxygen

--- misted/docs-only.py
+++ misted/docs-only.py
@@ -8,15 +8,11 @@
 
 @mainpage
 
-
-@section What is Misted?
-
 Welcome to Misted, the successor to Magnet. Misted, whose name may change without
 warning, is the Shiny! New! Way! to use AMQP from OOI code. It provides a
 twisted-compatible connection abstraction based on 'pockets', which are one
 letter different from sockets.
 
-@section Coming Attractions
 Much more docs on the way, keep an eye out at http://amoeba.ucsd.edu/doxygen/Misted
 
 """
